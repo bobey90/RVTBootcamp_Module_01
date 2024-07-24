b22cb6a31c0e75292e343f3e87d679707d5d0eec
mespadas <mespadas@BNJ-LA008>
2024-07-24 10:44:06 -0700

    Add project files.

---
+++ RVTBootcamp_Module_01/Command1.cs
@@ -0,0 +1,250 @@
+﻿namespace RVTBootcamp_Module_01
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // this is a variable for the Revit application
+            UIApplication uiapp = commandData.Application;
+
+            // this is a variable for the current Revit model
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            // Your code goes here
+
+            //create a comment using a double forward slash
+            //comments do not compile
+            //so you can leave yourself notes in your code
+
+            //Let's create some variables
+            //DataType VariableName = Value; <- always end the line with a semicolon!
+
+            string text1 = "this is my text";
+            string text2 = "this is my next text";
+
+            //combine strings
+            string text3 = text1 + text2;
+            string text4 = text1 + " " + text2 + "abcd";
+
+            //create number variables
+
+            int number1 = 10;
+            double number2 = 20.5;
+
+            //do some math
+            double number3 = number1 + number2;
+            double number4 = number1 - number2;
+            double number5 = number4 / 100;
+            double number6 = number5 * number4;
+            double number7 = (number6 + number5) / 100;
+
+            //convert meters to feet
+            double meters = 4;
+            double meterstofeet = meters * 3.28084;
+
+            //Convert mm to feet
+            double mm = 3500;
+            double mmTofeet = mm / 304.8;
+            double mmTofeet2 = (mm / 1000) * 3.28084;
+
+            //finding the remainder when dividing (ie. the modulo or mod)
+            double remainder1 = 100 % 10; // equals 0 (100 divided by 10 )
+            double remainder2 = 100 % 9;   //equals 1 (100 divided by 9 = 11 witha remainder of 1)
+
+            //increment a number by 1
+            number6++;
+            number6--;
+            number6 += 10;
+
+            //use conditional logic to compare things
+            // compare using boolean operators
+            // == equals
+            // != not equal
+            // > greater than
+            // < less than
+            // >= and <=
+
+            //check a value and perform a single action if true
+            if(number6 > 10)
+            {
+
+                //do something if true
+
+
+            }
+
+            //check a value nad perform an action if true and another if false
+
+            if(number5 == 100)
+            {
+                //do something if true
+            }
+            else
+            {
+                //do something if false
+            }
+
+            //check multiple calues and perform actions if true and false
+            if (number6 > 10)
+            {
+
+            }
+            else if (number6 == 8)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            //coumpound conditional statements
+            //look for two things (or more) using &&
+
+            if(number6 > 10 && number5 == 100)
+            {
+            //Do something if both are true
+            }
+
+            //look for either thing using ||
+            if(number6 > 10 || number5 == 100)
+            {
+                //do something if either is true
+            }
+
+            //create list
+
+            List<string> list1 = new List<string>();
+
+            // add items to list
+            list1.Add(text1);
+            list1.Add(text2);
+            list1.Add("this is some text");
+
+            //create list and add items to it - method 2
+            List<int> list2 = new List<int> { 1, 2, 3, 4, 5 };
+
+            //loop through a list using foreach loop
+            int letterCounter = 0;
+            foreach(string currentstring in list1)
+            {
+                //do something
+                //letterCounter = letterCounter + currentstring.Length;
+                letterCounter +=currentstring.Length;
+            }
+
+            //loop through a range of numbers
+            int numbercount = 0;
+            int counter = 100;
+            for(int i = 0; i <= counter; i++)
+            {
+                //do something
+                numbercount += i;
+            }
+
+            TaskDialog.Show("Number counter", "The number count is " + numbercount.ToString());
+
+            //create a transaction to lock the model
+            Transaction t = new Transaction(doc);
+            t.Start("Doing something in Revit");
+
+
+            //create a floor level - show in Revit API (www.revitapidocs.com)
+            //elevation value is in decimal feet regardless of model units
+            double elevation = 100;
+            Level newlevel = Level.Create(doc, elevation);
+            newlevel.Name = "My New Level";
+
+            //create a floor plan view - show in Revit API (www.revitapidoc.com)
+            //but first need to get a floor plan View Family Type
+            //by creating a Filtered Element Collector
+
+            FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+            collector1.OfClass(typeof(ViewFamilyType));
+
+            ViewFamilyType floorplanVFT = null;
+            foreach(ViewFamilyType curVFT in collector1)
+            {
+                if(curVFT.ViewFamily == ViewFamily.FloorPlan)
+                {
+                    floorplanVFT = curVFT;
+                }
+            }
+
+            //Create a view by specifying the document, view family type, and level
+
+            ViewPlan newPlan = ViewPlan.Create(doc, floorplanVFT.Id, newlevel.Id);
+            newPlan.Name = "My New floor Plan ";
+
+            //get ceiling plan view family type
+            ViewFamilyType ceilingPlanVFT = null;
+            foreach(ViewFamilyType curVFT in collector1)
+            {
+                if(curVFT.ViewFamily == ViewFamily.CeilingPlan)
+                {
+                    ceilingPlanVFT= curVFT;
+                }
+            }
+
+            //create a ceiling plan useing the cailing plan view family type
+            ViewPlan newCeilingPlan = ViewPlan.Create(doc, ceilingPlanVFT.Id, newlevel.Id);
+            newCeilingPlan.Name = "My new ceiling plan";
+
+            //create a sheet
+            //but first need to get title block
+            //by creating a Filtered Element Collector
+            FilteredElementCollector collector2 = new FilteredElementCollector(doc);
+            collector2.OfCategory(BuiltInCategory.OST_TitleBlocks);
+
+            //create a sheet
+            ViewSheet newSheet = ViewSheet.Create(doc, collector2.FirstElementId());
+            newSheet.Name = "My New Sheet";
+            newSheet.SheetNumber = "A101";
+
+            //add a view to a sheet using a Viewport - show in API
+            //First create a point
+            XYZ insPoint = new XYZ(1, 0.5, 0);
+
+            Viewport newViewport = Viewport.Create(doc,newSheet.Id, newPlan.Id, insPoint);
+
+
+            //make a change in the revit model
+
+
+
+            t.Commit();
+            t.Dispose();
+
+
+
+
+            return Result.Succeeded;
+        }
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand1";
+            string buttonTitle = "Button 1";
+            string? methodBase = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+
+            if (methodBase == null)
+            {
+                throw new InvalidOperationException("MethodBase.GetCurrentMethod().DeclaringType?.FullName is null");
+            }
+            else
+            {
+                Common.ButtonDataClass myButtonData1 = new Common.ButtonDataClass(
+                    buttonInternalName,
+                    buttonTitle,
+                    methodBase,
+                    Properties.Resources.Blue_32,
+                    Properties.Resources.Blue_16,
+                    "This is a tooltip for Button 1");
+
+                return myButtonData1.Data;
+            }
+        }
+    }
+
+}
---
+++ RVTBootcamp_Module_01/Module01Challenge.cs
@@ -0,0 +1,165 @@
+﻿using Autodesk.Revit.DB;
+
+namespace RVTBootcamp_Module_01
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Module01Challenge : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // this is a variable for the Revit application
+            UIApplication uiapp = commandData.Application;
+
+            // this is a variable for the current Revit model
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            // Your code goes here
+
+            /*
+             Declare a number variable and set it to 250
+
+            Declare a starting elevation variable and set it to 0
+
+            Declare a floor height variable and set it to 15 
+
+            Loop through the number 1 to the number variable
+
+            Create a level for each number
+
+            After creating the level, increment the current elevation by the floor height value.
+
+            If the number is divisible by 3, create a floor plan and name it "FIZZ_#"
+
+            If the number is divisible by 5, create a ceiling plan and name it "BUZZ_#"
+
+            If the number is divisible by both 3 and 5, create a sheet and name it "FIZZBUZZ_#"
+
+                A few hints:
+
+            Use a For loop to loop through the numbers
+
+            Use the modulus operator ( % ) to check if the number is divisible by 3 or 5
+
+            You can check two conditions in a conditional statement by using && (ie. If(a == 5 && b == 10)
+
+            Don't forget your transaction!
+
+            Reference the logic diagram below to see a flow chart of the add-in.
+            */
+
+
+            //create a transaction to lock the model
+            Transaction t = new Transaction(doc);
+            t.Start("Doing something in Revit");
+
+            int level_number = 250;
+
+            double starting_elevation = 0;
+            double floor_height = 15;
+            double remainder_3;
+            double remainder_5;
+
+            
+            //string buzz = "Buzz ";
+
+            Level NewLevel;
+            
+            FilteredElementCollector collector_titleblock;
+            FilteredElementCollector collector_plan;
+            
+            ViewPlan new_Mod_Plan;
+            ViewSheet newSheet;
+            ViewFamilyType floorplanVFT;
+
+            for (int i = 1; i <= level_number; ++i)
+            {
+
+                NewLevel = Level.Create(doc, starting_elevation);
+                NewLevel.Name = "Level " + i;
+                starting_elevation += floor_height;
+
+                remainder_3 = i % 3;
+                remainder_5 = i % 5;
+
+
+
+                if (remainder_3 == 0 && remainder_5 == 0)
+                {
+                    collector_titleblock = new FilteredElementCollector(doc);
+                    collector_titleblock.OfCategory(BuiltInCategory.OST_TitleBlocks);
+
+                    newSheet = ViewSheet.Create(doc, collector_titleblock.FirstElementId());
+                    newSheet.Name = "FizzBuzz" + i;
+                    newSheet.SheetNumber = "A10" + i;
+
+                }
+
+                if (remainder_3 == 0)
+                {
+
+
+
+                    collector_plan = new FilteredElementCollector(doc);
+                    collector_plan.OfClass(typeof(ViewFamilyType));
+
+                    floorplanVFT = null;
+                    foreach (ViewFamilyType curVFT in collector_plan)
+                    {
+                        if (curVFT.ViewFamily == ViewFamily.FloorPlan)
+                        {
+                            floorplanVFT = curVFT;
+
+                        }
+                        new_Mod_Plan = ViewPlan.Create(doc, floorplanVFT.Id, NewLevel.Id);
+                        new_Mod_Plan.Name = "Fizz " + i;
+
+                    }
+                }
+
+
+
+            }
+
+
+
+
+
+            //make a change in the revit model
+
+
+
+            t.Commit();
+        t.Dispose();
+
+
+
+
+        return Result.Succeeded;
+        }
+        internal static PushButtonData GetButtonData()
+        {
+        // use this method to define the properties for this command in the Revit ribbon
+        string buttonInternalName = "btnCommand1";
+        string buttonTitle = "Button 1";
+        string? methodBase = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+
+        if (methodBase == null)
+        {
+            throw new InvalidOperationException("MethodBase.GetCurrentMethod().DeclaringType?.FullName is null");
+        }
+        else
+        {
+            Common.ButtonDataClass myButtonData1 = new Common.ButtonDataClass(
+                buttonInternalName,
+                buttonTitle,
+                methodBase,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 1");
+
+            return myButtonData1.Data;
+        }
+        }
+        }
+
+        }
